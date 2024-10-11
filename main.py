508ac92569bdc998ebb523c4b2848881b8c5aec6
PlumStone70486 <tsbg95@gmail.com>
2024-10-11 14:23:26 +0200

    created count words and count letters

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-letters = {"abcdefghijklmnopqrstuvwxyz"}
+
+def letter_count(text):
+    lower_case = text.lower()
+    letters = {}
+    for i in lower_case:
+        if i.isalpha():
+            if i not in letters:
+                letters[i] = 0
+            letters[i] += 1
+    return letters
+
 with open ("books/frankenstein.txt") as f:
     file_contents = f.read()
 
-def word_counting():
-    words = file_contents.split()
-    word_count = words.__len__()
+char_count = letter_count(file_contents)
+word_count = len(file_contents.split())
 
-#def letter_count():
-    #lower_case = file_contents.lower()
-    #count_letters = lower_case.__len__(letters)
-
-    #print(count_letters)
-def main():
-    return word_counting
+print(f"Word count is: {word_count}")
+print(char_count)
